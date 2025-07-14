0132e8c4a22a51c048753760aa89e046c7a428f0
parivgabriela <pari.gabriela@gmail.com>
2025-07-14 13:47:56 -0300

    add a verification if the folder of logs doesn't exist the program will create a new one, this will resolve inn case someone clone the repo

--- asistente-tec-ia/app/config/logging_config.py
+++ asistente-tec-ia/app/config/logging_config.py
@@ -1,8 +1,21 @@
+import os
 import logging
 from logging.handlers import RotatingFileHandler
 
 def setup_logging():
     LOG_DIRECTORY = "logs/app.log"
+    
+    log_path = LOG_DIRECTORY
+    log_dir = os.path.dirname(log_path)
+
+    # Crear directorio si no existe
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+        # Crear archivo vacío si no existe
+    if not os.path.exists(log_path):
+        open(log_path, 'a').close()
+    
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
     
     handler = RotatingFileHandler(LOG_DIRECTORY, maxBytes=100000, backupCount=3)
