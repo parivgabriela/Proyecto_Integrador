900d1aefdba1ce82045d9b1d852b89488b5f2186
parivgabriela <pari.gabriela@gmail.com>
2025-07-23 15:04:27 -0300

    revert changes, the commit #8 was only for index template

--- asistente-tec-ia/app/__init__.py
+++ asistente-tec-ia/app/__init__.py
@@ -3,14 +3,13 @@ from flask import Flask
 from .config.config import Config
 from .routes.tec_ia_rutas import tec_ia_bot  
 from .config.logging_config import setup_logging
-from .extensions import socketio, clean_upload_folder
-from .services.constants_process import UPLOAD_USER_PATH
+from .extensions import socketio
 
+UPLOAD_USER_PATH = 'uploads/'
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    clean_upload_folder(UPLOAD_USER_PATH)
     app.config['UPLOAD_FOLDER'] = UPLOAD_USER_PATH
 
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
--- asistente-tec-ia/app/extensions.py
+++ asistente-tec-ia/app/extensions.py
@@ -1,22 +1,3 @@
-import os
-import shutil
 from flask_socketio import SocketIO
 
 socketio = SocketIO()
-
-def clean_upload_folder(UPLOAD_FOLDER):
-    if os.path.exists(UPLOAD_FOLDER):
-        print(f"Limpiando la carpeta de uploads: {UPLOAD_FOLDER}")
-        for filename in os.listdir(UPLOAD_FOLDER):
-            file_path = os.path.join(UPLOAD_FOLDER, filename)
-            try:
-                if os.path.isfile(file_path) or os.path.islink(file_path):
-                    os.unlink(file_path) # Elimina archivos y enlaces simbólicos
-                elif os.path.isdir(file_path):
-                    shutil.rmtree(file_path) # Elimina subdirectorios
-            except Exception as e:
-                print(f'Error al eliminar {file_path}: {e}')
-    else:
-        print(f"La carpeta de uploads no existe: {UPLOAD_FOLDER}. Creándola...")
-        os.makedirs(UPLOAD_FOLDER)
-
