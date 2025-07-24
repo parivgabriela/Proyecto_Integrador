491a4a3fc8c9814b2a22fb5b9702925d1c1c4c19
parivgabriela <pari.gabriela@gmail.com>
2025-07-24 13:08:33 -0300

    #10 reestructure of logs, the code it is located in app/config/loggin_coinfig.py

--- asistente-tec-ia/app/config/logging_config.py
+++ asistente-tec-ia/app/config/logging_config.py
@@ -1,25 +1,46 @@
 import os
+import sys
 import logging
 from logging.handlers import RotatingFileHandler
 
+from .logging_handler import NoSocketIOFilter
+
 def setup_logging():
-    LOG_DIRECTORY = "logs/app.log"
-    
-    log_path = LOG_DIRECTORY
-    log_dir = os.path.dirname(log_path)
+    LOG_FILE_PATH = "logs/app.log"
+    log_dir = os.path.dirname(LOG_FILE_PATH)
 
     # Crear directorio si no existe
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
+    os.makedirs(log_dir, exist_ok=True)
 
-        # Crear archivo vacío si no existe
-    if not os.path.exists(log_path):
-        open(log_path, 'a').close()
-    
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
-    
-    handler = RotatingFileHandler(LOG_DIRECTORY, maxBytes=100000, backupCount=3)
-    handler.setLevel(logging.INFO)
-    handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
 
-    logging.getLogger().addHandler(handler)
+    # Limpiar cualquier handler existente para evitar duplicados o conflictos
+    if logger.hasHandlers():
+        logger.handlers.clear()
+
+    no_socketio_filter = NoSocketIOFilter()
+    
+    file_handler = RotatingFileHandler(
+        LOG_FILE_PATH, 
+        maxBytes=100000, 
+        backupCount=3,
+        encoding='utf-8'
+    )
+
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
+    file_handler.addFilter(no_socketio_filter)
+
+    # Crear el handler para mostrar en la consola (StreamHandler)
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s: %(message)s"))
+    console_handler.addFilter(no_socketio_filter)
+
+
+    # Añadir ambos handlers al logger raíz
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    logging.info("El sistema de logging ha sido configurado.")
